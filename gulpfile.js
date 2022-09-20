c45b364387e6c69099dc69f65a826a3e3385f2fb
Yerlan Altynbekov <erlannasyrov@gmail.com>
2022-09-20 21:14:40 +0600

    finish comment-section

--- gulpfile.js
+++ gulpfile.js
@@ -56,6 +56,7 @@ function svgSprites() {
 function scripts() {
   return src([
       'node_modules/jquery/dist/jquery.js',
+      'node_modules/rateyo/src/jquery.rateyo.js',
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
